94ef7a5012434530d8d870e948c2f7a2174574c7
MarkZither <mark.burton@zither-it.co.uk>
2020-12-12 00:16:19 +0100

    use netcoreapp3.1

--- build/Build.cs
+++ build/Build.cs
@@ -55,7 +55,7 @@ partial class Build : NukeBuild
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion(Framework = "net5.0")] readonly GitVersion GitVersion;
+    [GitVersion(Framework = "netcoreapp3.1")] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
